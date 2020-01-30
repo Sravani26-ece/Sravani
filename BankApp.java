5d46ee7c6691e067247781b3df15fba6dcae9c95
Sravani26-ece <59988751+Sravani26-ece@users.noreply.github.com>
2020-01-30 16:36:41 +0530

    Create BankApp.java

---
+++ BankApp.java
@@ -0,0 +1,36 @@
+package com.inventrix.bank;
+
+public class BankApp {
+
+	private String cName;
+	private String gender;
+	private String id;
+	private int balance;
+	
+	public String getcName() {
+		return cName;
+	}
+	public void setcName(String cName) {
+		this.cName = cName;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public int getBalance() {
+		return balance;
+	}
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+	
+
+}
